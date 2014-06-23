dee2ae680f0a92c760769ffd24b79728573bccd3
rekado <rekado@elephly.net>
2014-06-23 08:06:05 +0200

    disco: support dynamic and per-node identities

--- lib/libertree/server/disco.rb
+++ lib/libertree/server/disco.rb
@@ -7,28 +7,38 @@ module Libertree
       extend Libertree::Server::Responder::Helper
 
       private
-      @@identities = []
-      @@features = [ "http://jabber.org/protocol/disco#info" ]
+      @@identities = {}
+      @@features = {}
 
       def self.disco_info(stanza)
         info = stanza.reply
-        info.identities = @@identities
-        info.features = @@features
+        info.node = stanza.node
+
+        # when node=:rule, the identity is considered a proc to be evaluated for every node
+        info.identities = @@identities[stanza.node] + @@identities[:rule].map{|rule| rule.call(stanza.node)}.compact
+        info.features = @@features[stanza.node]
+
         @client.write info
       end
 
       public
-      def self.register_feature(feature)
-        @@features << feature
+      def self.register_feature(feature, node=nil)
+        @@features[node] << feature
       end
 
-      def self.register_identity(identity)
-        @@identities << identity
+      def self.register_identity(identity, node=nil)
+        @@identities[node] << identity
       end
 
       def self.init(client)
         # set @client for the `respond` helper method
         @client = client
+
+        # return a new empty array for each unknown key
+        @@identities.default_proc = proc {|h,k| h[k] = []}
+        @@features.default_proc = proc {|h,k| h[k] = []}
+
+        self.register_feature "http://jabber.org/protocol/disco#info"
         client.register_handler(:disco_info) {|stanza| disco_info(stanza)}
       end
     end
