cce5f8345e0578e9f21f5919cd1e22d88076b42d
Pistos <git dawt pistos aet purepistos dawt net>
2016-02-14 09:53:53 -0500

    Only drop test DB if it exists.

--- bin/test.sh
+++ bin/test.sh
@@ -6,7 +6,7 @@ export LIBERTREE_ENV=test
 export LIBERTREE_DB=${LIBERTREE_DB:-libertree_test}
 
 # TODO: Safeguard against dropping production DB.  Check for DB name "libertree".
-dropdb -U postgres ${LIBERTREE_DB}
+dropdb -U postgres --if-exists ${LIBERTREE_DB}
 createdb -U postgres -O libertree ${LIBERTREE_DB}
 ${SCRIPT_DIR}/../../libertree-db/migrate.sh
 bundle exec "rspec -f d $@"
